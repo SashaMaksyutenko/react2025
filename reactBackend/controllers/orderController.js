0fd180608bc8f293231090554b4c03dc72047a0c
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2025-03-26 17:41:14 +0200

    Display order details in Admin panel

--- reactBackend/controllers/orderController.js
+++ reactBackend/controllers/orderController.js
@@ -24,16 +24,18 @@ const placeOrder = async (req, res) => {
   }
 }
 // Placing orders using Stripe Method
-const placeOrderStripe = async (req, res) => {
-  
-}
+const placeOrderStripe = async (req, res) => {}
 // Placing orders using Razorpay Method
-const placeOrderRazorpay = async (req, res) => {
-  
-}
+const placeOrderRazorpay = async (req, res) => {}
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
- 
+  try {
+    const orders = await orderModel.find({})
+    res.json({ success: true, orders })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
 }
 // User Order Data For Forntend
 const userOrders = async (req, res) => {
@@ -47,14 +49,21 @@ const userOrders = async (req, res) => {
   }
 }
 // update order status from Admin Panel
-const updateStatus = async (req,res) => {
-    try {
-        const { orderId, status } = req.body
-        await orderModel.findByIdAndUpdate(orderId, { status })
-        res.json({success:true,message:'Status Updated'})
-    } catch (error) {
-        console.log(error)
-        res.json({success:false,message:error.message})
-    }
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body
+    await orderModel.findByIdAndUpdate(orderId, { status })
+    res.json({ success: true, message: 'Status Updated' })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
 }
-export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus
+}
